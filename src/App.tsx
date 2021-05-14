6318495e614fcd903fbeccbece7eebd32ee98e3e
Jorgen <jorgen@buildnode.io>
2021-05-14 16:42:32 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css';
+import '@fontsource/roboto-mono';
+
+const App:React.FC = () => {
+  return (
+    <main>
+      <header className="splash">
+        <main>
+          <div className="hero-emoji">👋</div>
+          <p>
+            Hey there, we're <span className="buildmoji" /> Buildnode Digital, and we build 🔥 web products.
+          </p>
+          <p>
+            ✉️ <a href="mailto:newbusiness@buildnode.io">newbusiness@buildnode.io</a>
+          </p>
+        </main>
+        <footer>
+          Hosted on the Interplanetary File System, or IPFS.
+          (<a href="#">What’s that?</a> 🤔)
+        </footer>
+      </header>
+    </main>
+  );
+}
+
+export default App;
